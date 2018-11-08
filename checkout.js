50af4fbdce632b048ece580f009d38c9d494f341
DVintt <vincealday@gmail.com>
2018-11-08 02:04:04 -0800

    Can now remove and edit quantity for local storage items

--- checkout.js
+++ checkout.js
@@ -1,27 +1,52 @@
 //Local Storage: Cart Data
 var cartData = JSON.parse(sessionStorage.getItem('cart'));
-console.log(cartData);
+console.log("cart data length =" + cartData.length);
 
 //Handlebars Variables
 var source = $("#confirmTemplate").html();
 var template = Handlebars.compile(source);
 var parentDiv = $("#confirmation");
 
-//Prints out current cart list
-for (var i = 0; i < cartData.length; i++){
+
+// No Items Message + confirmation button
+if(cartData.length != null || cartData.length != 0){
+    $("#noItems").css('visibility','hidden'); 
+
+    //Prints out current cart list
+    for (var i = 0; i < cartData.length; i++){
     var curData = cartData[i];
     var curHtml = template(curData);
     parentDiv.append(curHtml);
 }
 
+}
+else{
+    $("#noItems").css('visibility','visible'); 
+    $("#itemPresent").css('visibility','hidden');
+}
+
 /*Removes Selected Item and hides no items text(If Applicable)*/
 $("#confirmation").on("click",'.removeItem',function(){
-    $(this).parent('td').parent('tr').remove();
-    console.log(colCount);
-    if(colCount>1){
-        $("#noItems").css('visibility','hidden');
+    // Retrieving cart
+    var cartItem = JSON.parse(sessionStorage.getItem('cart'));
+    
+    //Looks for item in the storage removes it
+    for(var i = 0; i < cartItem.length; i++){
+        if(cartItem[i].dishname == $(this).attr("name")){
+            console.log("Before: " + cartItem.length);
+            cartItem.splice(i,1);
+            break;
+        }
     }
-    else{
-        $("#noItems").css('visibility','visible');
-    }
+    
+    // Updating Storage
+    sessionStorage.setItem('cart',JSON.stringify(cartItem));
+    $(this).parent('div').parent('td').parent('tr').remove();
+    
+    // No Items are in the cart Message + Removes confirmation button and instructions
+    if(cartItem.length == 0){
+        $("#noItems").css('visibility','visible'); 
+        $("#itemPresent").remove();
+    } 
+    
 });
--- menu.js
+++ menu.js
@@ -2,14 +2,21 @@
 var cartData = JSON.parse(sessionStorage.getItem('cart') ) || [];
 
 //List of Items in Cart
-var colCount = $('tr #cartList').children('td').length; 
+var colCount = $('#cartList').children('td').length; 
 
 //HandleBar Variables
 var source = $("#cartTemplate").html();
 var template = Handlebars.compile(source);
 var parentDiv = $("#cartList");
 
-//Outputs current items in cart
+// No Items Message
+if(cartData.length == 0){
+    $("#noItems").css('visibility','visible');
+} 
+else{
+    $("#noItems").css('visibility','hidden');
+}
+
 //Prints out current cart list
 for (var i = 0; i < cartData.length; i++){
     var curData = cartData[i];
@@ -24,9 +31,22 @@ $("#menu").on("click",'.addItem',function(){
     
     // Getting Name of Dish, Quantity Selected, and Img of Dish
     var dish = $(this).siblings('div').children('input').attr("name");
-    var quant = $(this).siblings('div').children('input').attr("value");
+    var quant = $(this).siblings('div').children('input').val();
     var img = "../Images/" + dish + ".jpg";
     
+
+    // Checking if Item already exist in cart
+    for(var i = 0; i < cart.length; i++){
+        if(cart[i].dishname == $(this).attr("name")){
+            var itemQuant = parseInt(cart[i].quantity) + parseInt(quant);
+            cart[i].quantity = itemQuant;
+            
+            //Updating Quantity and cancels function
+            sessionStorage.setItem('cart',JSON.stringify(cart));
+            return;
+        }
+    }
+    console.log("test break");
     //Creating new Object
     var newItem = {'dishname':dish,
                 'dishimg':img,'quantity':quant};
@@ -42,31 +62,62 @@ $("#menu").on("click",'.addItem',function(){
     var parentDiv = $("#cartList");
     var html = template(cartData);
     parentDiv.append(html)     
-});
 
-
-
-    console.log(colCount);
-/*Removes Selected Item and hides no items text(If Applicable)*/
-$("#cartList").on("click",'.removeItem',function(){
-    $(this).parent('td').remove();
-    console.log(colCount);
-    if(colCount>1){
+    if(cart.length > 0){
         $("#noItems").css('visibility','hidden');
     }
-    else{
-        $("#noItems").css('visibility','visible');
+    console.log("cart Length = " + cart.length);
+    console.log("cartData Length = " + cartData.length);
+});
+
+/* Removes Selected Item and hides no items text(If Applicable) */
+$("#cartList").on("click",'.removeItem',function(){
+    // Retrieving cart
+    var cartItem = JSON.parse(sessionStorage.getItem('cart'));
+    
+    //Looks for item in the storage removes it
+    for(var i = 0; i < cartItem.length; i++){
+        if(cartItem[i].dishname == $(this).attr("name")){
+            console.log("Before: " + cartItem.length);
+            cartItem.splice(i,1);
+            break;
+        }
     }
+    
+    // Updating Storage
+    sessionStorage.setItem('cart',JSON.stringify(cartItem));
+    $(this).parent('td').remove();
+
+    console.log("After: " + cartItem.length);
+
+    // No Items are in the cart Message
+    if(cartItem.length == 0){
+        $("#noItems").css('visibility','visible');      
+    } 
 });
 
-//Change quantity of item (Still in progress)
-$("#cart").on("click",'.chageQuant',function(){
-    $(this).closest('input').val();
+/* Change quantity of item (Still in progress) */
+$("#cart").on("click",'.changeQuant',function(){
+    // Retrieving Cart and current input value
+    var cartItem = JSON.parse(sessionStorage.getItem('cart'));  
+    var newQuant = $(this).siblings('div').children('input').val();
+    
+    //Looks for item in the storage and updates its quantity value
+    for(var i = 0; i < cartItem.length; i++){
+        if(cartItem[i].dishname == $(this).attr("name")){
+            cartItem[i].quantity = newQuant;
+            break;
+        }
+    }
+
+    //Updating Storage
+    sessionStorage.setItem('cart',JSON.stringify(cartItem));
 });
 
 
 
-/* 
+
+/* Original Cart Template
 <td> 
                         <h3 style="text-align: center">Dish 1</h3>
                         <img style="margin-left:auto;margin-right:auto" src="../Images/vietnamese-pho.jpg" alt="burger" height="100px" width="150px">
