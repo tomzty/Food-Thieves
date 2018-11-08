20a21b08672c363426aae761055d9dd1003f5150
Tianyu Zhang <tiz074@ucsd.edu>
2018-11-08 15:04:45 -0800

    Add login Page, stuff in css and new js file.

---
+++ script1.js
@@ -0,0 +1,25 @@
+var attempt = 3; //Variable to count number of attempts
+
+//Below function Executes on click of login button
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if ( username == "FoodThieves" && password == "FoodThieves"){
+		alert ("Login successfully");
+		window.location = "http://google.com"; //redirecting to other page
+		return false;
+	}
+	else{
+		attempt --;//Decrementing by one
+		alert("You have left "+attempt+" attempt;");
+		
+		//Disabling fields after 3 attempts
+		if( attempt == 0){
+			document.getElementById("username").disabled = true;
+			document.getElementById("password").disabled = true;
+			document.getElementById("submit").disabled = true;
+			return false;
+		}
+	}
+}
