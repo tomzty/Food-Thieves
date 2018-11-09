9bc1cf640823f1dba48f39904d504aa060b93e6d
henn-g <haguerra@ucsd.edu>
2018-11-08 16:56:02 -0800

    adding fake data JSON struct

---
+++ FOOD_DATA.js
@@ -0,0 +1,17 @@
+// JSON initialization of global objects to be used and manipulated
+// This is essentially our fake backend data
+
+var restaurantData = [
+    {
+        'restaurant': 'Rakki Rakki',
+        'dishes': [ 
+            {'Black Edition Ramen': 13.00}, 
+            {'Salmon Skin Sushi': 10.00}, 
+            {'Miso Soup': 3.00} 
+        ],
+        'hasAllergens': false  
+    },
+    {
+        
+    } 
+]
