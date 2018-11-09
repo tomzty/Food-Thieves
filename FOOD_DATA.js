932bef5b5732f053507de0a63cf8a95e9e83c5c6
henn-g <haguerra@ucsd.edu>
2018-11-08 17:12:55 -0800

    completed fake data struct

--- FOOD_DATA.js
+++ FOOD_DATA.js
@@ -1,5 +1,7 @@
 // JSON initialization of global objects to be used and manipulated
 // This is essentially our fake backend data
+// Quick side note: if the hasAllergens value is set to TRUE
+// then the ui will indicate a tiny image marker for the user to see
 
 var restaurantData = [
     {
@@ -12,6 +14,39 @@ var restaurantData = [
         'hasAllergens': false  
     },
     {
-        
-    } 
+        'restaurant': 'Nishiki',
+        'dishes': [ 
+            {'Tonkotsu Ramen': 13.00}, 
+            {'Tempura Sushi': 10.00}, 
+            {'Miso Soup': 3.00} 
+        ],
+        'hasAllergens': true 
+    },
+    {
+    'restaurant': 'In n Out',
+        'dishes': [ 
+            {'Hamburger': 6.00}, 
+            {'Cheese Burger': 6.00}, 
+            {'Animal Style Fries': 3.00} 
+        ],
+        'hasAllergens': true
+    },
+    {
+        'restaurant': 'Mana KBBQ',
+        'dishes': [ 
+            {'A1 combo': 20.00}, 
+            {'A2 combo': 24.00}, 
+            {'Family combo': 30.00} 
+        ],
+        'hasAllergens': true  
+    }, 
+    {
+        'restaurant': 'Dumpling House',
+        'dishes': [ 
+            {'Soup Dumplings': 10.00}, 
+            {'Chow Mein': 8.00}, 
+            {'Wonton Soup': 6.00} 
+        ],
+        'hasAllergens': false  
+    }
 ]
