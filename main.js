b2ce36a06955e68865385d77eb86b1ba52411f57
Vince Alday <vincealday@gmail.com>
2018-12-01 02:43:22 -0800

    Added Framework for final Design and fixed minor bugs

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 /* @@@@@ School Selection @@@@@ */
 console.log('hello')
-function goToNewPage(){
-  var e = document.getElementById("dropDownId");
-  var choice = e.value
-  console.log(choice)
-  window.open(choice)
+
+function goToNewPage() {
+	var e = document.getElementById("dropDownId");
+	var choice = e.value
+	console.log(choice)
+	window.open(choice)
 }
 
 /* @@@@@ Modal @@@@@ */
 $("#recentOrders").on("click", function () {
-  $(".modal").css('display', 'block');
+	$(".modal").css('display', 'block');
 });
 
 $(".close").on("click", function () {
-  $(".modal").css('display', 'none')
+	$(".modal").css('display', 'none')
 });
 
 // When the user clicks anywhere outside of the modal, close it
-$(window).click(function(event) {
-  if (event.target == $(".modal")) {
-      $(".modal").css("display","none");
-  }
+$(window).click(function (event) {
+	if (event.target == $(".modal")) {
+		$(".modal").css("display", "none");
+	}
 });
 
 
@@ -28,17 +29,16 @@ $(window).click(function(event) {
 var user = sessionStorage.getItem('user');
 console.log(" test: " + sessionStorage.getItem('user'));
 // Replaces placeholder name
-if(user != null){
-    $(".user").html('<a href="Views/profilePage.html">Profile</a>');
-    console.log("hi");
-}
-else{
-    $(".user").html('<a href="login.html">Log In</a>');
+if (user != null) {
+	$(".user").html('<a href="Views/profilePage.html">Profile</a>');
+	console.log("hi");
+} else {
+	$(".user").html('<a href="login.html">Log In</a>');
 }
 
 // Sends user to page based on option
-var makePath = function(form) {
-    form.action = document.getElementById('dropDownId').value;
+var makePath = function (form) {
+	form.action = document.getElementById('dropDownId').value;
 };
 
 
@@ -54,22 +54,19 @@ var parentDiv = $("#confirmation");
 
 
 // No Items Message + confirmation button   
-if(validCart != null){
-    $("#noItems").css('visibility','hidden'); 
+if (validCart != null) {
+	$("#noItems").css('visibility', 'hidden');
 
-    //Prints out current cart list
-    for (var i = 0; i < cartData.length; i++){
-        var curOrder = cartData[i].prevOrder.split("");
+	//Prints out current cart list
+	for (var i = cartData.length - 1; i >= 0; i--) {
+		var curOrder = cartData[i].prevOrder.split("");
 
-        var curData = cartData[i];
-        var curHtml = template(curData);
-        parentDiv.append(curHtml);
+		var curData = cartData[i];
+		var curHtml = template(curData);
+		parentDiv.append(curHtml);
+	}
+
+} else {
+	$("#noItems").css('visibility', 'visible');
+	$("#itemPresent").css('visibility', 'hidden');
 }
-
-}
-else{
-    $("#noItems").css('visibility','visible'); 
-    $("#itemPresent").css('visibility','hidden');
-}
-
-
--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 //Local Storage: Cart Data
-var cartData = JSON.parse(sessionStorage.getItem('cart') ) || [];
+var cartData = JSON.parse(sessionStorage.getItem('cart')) || [];
 
 //List of Items in Cart
-var colCount = $('#cartList').children('td').length; 
+var colCount = $('#cartList').children('td').length;
 
 //HandleBar Variables
 var source = $("#cartTemplate").html();
@@ -10,116 +10,118 @@ var template = Handlebars.compile(source);
 var parentDiv = $("#cartList");
 
 // No Items Message
-if(cartData.length == 0){
-    $("#noItems").css('visibility','visible');
-} 
-else{
-    $("#noItems").css('visibility','hidden');
+if (cartData.length == 0) {
+	$("#noItems").css('visibility', 'visible');
+} else {
+	$("#noItems").css('visibility', 'hidden');
 }
 
 //Prints out current cart list
-for (var i = 0; i < cartData.length; i++){
-    var curData = cartData[i];
-    var curHtml = template(curData);
-    parentDiv.append(curHtml);
+for (var i = 0; i < cartData.length; i++) {
+	var curData = cartData[i];
+	var curHtml = template(curData);
+	parentDiv.append(curHtml);
 }
 
 /* Adds Item to cart and appends it to html */
-$("#menu").on("click",'.addItem',function(){
-    //Retrieving exisiting cart(If it exists)
-    var cart = JSON.parse(sessionStorage.getItem('cart')) || [];
-    
-    // Getting Name of Dish, Quantity Selected, and Img of Dish
-    var dish = $(this).siblings('div').children('input').attr("name");
-    var quant = $(this).siblings('div').children('input').val();
-    var img = "../Images/" + dish + ".jpg";
-    
+$("#menu").on("click", '.addItem', function () {
+	//Retrieving exisiting cart(If it exists)
+	var cart = JSON.parse(sessionStorage.getItem('cart')) || [];
 
-    // Checking if Item already exist in cart
-    for(var i = 0; i < cart.length; i++){
-        if(cart[i].dishname == $(this).attr("name")){
-            var itemQuant = parseInt(cart[i].quantity) + parseInt(quant);
-            cart[i].quantity = itemQuant;
-            
-            //Updating Quantity and cancels function
-            sessionStorage.setItem('cart',JSON.stringify(cart));
-            return;
-        }
-    }
-    console.log("test break");
-    //Creating new Object
-    var newItem = {'dishname':dish,
-                'dishimg':img,'quantity':quant};
-    
-    //Added new item to cart and saving to local storage
-    cart.push(newItem);
-    sessionStorage.setItem('cart',JSON.stringify(cart));
-    
-    //Adding Item to current cart list
-    var source = $("#cartTemplate").html();
-    var template = Handlebars.compile(source);
+	// Getting Name of Dish, Quantity Selected, and Img of Dish
+	var dish = $(this).siblings('div').children('input').attr("name");
+	var quant = $(this).siblings('div').children('input').val();
+	var img = "../Images/" + dish + ".jpg";
 
-    var parentDiv = $("#cartList");
-    var html = template(cartData);
-    parentDiv.append(html)     
 
-    if(cart.length > 0){
-        $("#noItems").css('visibility','hidden');
-    }
-    console.log("cart Length = " + cart.length);
-    console.log("cartData Length = " + cartData.length);
+	// Checking if Item already exist in cart
+	for (var i = 0; i < cart.length; i++) {
+		if (cart[i].dishname == $(this).attr("name")) {
+			var itemQuant = parseInt(cart[i].quantity) + parseInt(quant);
+			cart[i].quantity = itemQuant;
+
+			//Updating Quantity and cancels function
+			sessionStorage.setItem('cart', JSON.stringify(cart));
+			return;
+		}
+	}
+	console.log("test break");
+	//Creating new Object
+	var newItem = {
+		'dishname': dish,
+		'dishimg': img,
+		'quantity': quant
+	};
+
+	//Added new item to cart and saving to local storage
+	cart.push(newItem);
+	sessionStorage.setItem('cart', JSON.stringify(cart));
+
+	//Adding Item to current cart list
+	var source = $("#cartTemplate").html();
+	var template = Handlebars.compile(source);
+
+	var parentDiv = $("#cartList");
+	var html = template(cartData);
+	parentDiv.append(html)
+
+	if (cart.length > 0) {
+		$("#noItems").css('visibility', 'hidden');
+	}
+	console.log("cart Length = " + cart.length);
+	console.log("cartData Length = " + cartData.length);
 });
 
 /* Removes Selected Item and hides no items text(If Applicable) */
-$("#cartList").on("click",'.removeItem',function(){
-    // Retrieving cart
-    var cartItem = JSON.parse(sessionStorage.getItem('cart'));
-    
-    //Looks for item in the storage removes it
-    for(var i = 0; i < cartItem.length; i++){
-        if(cartItem[i].dishname == $(this).attr("name")){
-            console.log("Before: " + cartItem.length);
-            cartItem.splice(i,1);
-            break;
-        }
-    }
-    
-    // Updating Storage
-    sessionStorage.setItem('cart',JSON.stringify(cartItem));
-    $(this).parent('td').parent('tr').remove();
+$("#cartList").on("click", '.removeItem', function () {
+	// Retrieving cart
+	var cartItem = JSON.parse(sessionStorage.getItem('cart'));
 
-    console.log("After: " + cartItem.length);
+	//Looks for item in the storage removes it
+	for (var i = 0; i < cartItem.length; i++) {
+		if (cartItem[i].dishname == $(this).attr("name")) {
+			console.log("Before: " + cartItem.length);
+			cartItem.splice(i, 1);
+			break;
+		}
+	}
 
-    // No Items are in the cart Message
-    if(cartItem.length == 0){
-        $("#noItems").css('visibility','visible');      
-    } 
+	// Updating Storage
+	sessionStorage.setItem('cart', JSON.stringify(cartItem));
+	$(this).parent('td').parent('tr').remove();
+
+	console.log("After: " + cartItem.length);
+
+	// No Items are in the cart Message
+	if (cartItem.length == 0) {
+		$("#noItems").css('visibility', 'visible');
+	}
 });
 
 /* Change quantity of item (Still in progress) */
-$('#cart').on("click",".changeQuant",function(){
-    // Retrieving Cart and current input value
-    var cartItem = JSON.parse(sessionStorage.getItem('cart'));  
-    var newQuant = $(this).siblings('div').children('input').val();
-   // var dishName = $(this).attr("name");
-    //var quantName = "#quant" + dishName;
-    //var newQuant = $(quantName).children('input').val();
-   // console.log("dishname =" + dish);
-   // console.log("Current Quant "+newQuant);
-    //var currentItem = $('input[name="ramen"]').name;
-    //var currentItem = $('input[name="' + dish + '"]');
-   // console.log("currentItem =" + currentItem );
-    //Looks for item in the storage and updates its quantity value
-    for(var i = 0; i < cartItem.length; i++){
-        if(cartItem[i].dishname == $(this).attr("name")){
-            cartItem[i].quantity = newQuant;
-            console.log("hello");
-            break;
-        }
-    }
+$('#cart').on("click", ".changeQuant", function () {
+	// Retrieving Cart and current input value
+	var cartItem = JSON.parse(sessionStorage.getItem('cart'));
+	var newQuant = $(this).siblings('div').children('input').val();
+	// var dishName = $(this).attr("name");
+	//var quantName = "#quant" + dishName;
+	//var newQuant = $(quantName).children('input').val();
+	// console.log("dishname =" + dish);
+	// console.log("Current Quant "+newQuant);
+	//var currentItem = $('input[name="ramen"]').name;
+	//var currentItem = $('input[name="' + dish + '"]');
+	// console.log("currentItem =" + currentItem );
+	//Looks for item in the storage and updates its quantity value
+	for (var i = 0; i < cartItem.length; i++) {
+		if (cartItem[i].dishname == $(this).attr("name")) {
+			cartItem[i].quantity = newQuant;
+			console.log("hello");
+			break;
+		}
+	}
 
-    //Updating Storage
-    sessionStorage.setItem('cart',JSON.stringify(cartItem));
+	//Updating Storage
+	sessionStorage.setItem('cart', JSON.stringify(cartItem));
 });
 
 
